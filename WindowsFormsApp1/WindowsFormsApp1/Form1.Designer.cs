2673e534d2f6b5afcfe152a889283a96cbdc2757
vahid PC OFFICE <vahid.zahani@gmail.com>
2023-11-21 10:35:56 +0330

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1189, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "vahid.zahani@gmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "checking in case\\gozaresh";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(788, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 276);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "report creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Word=Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+using System.Threading;
+using System.Linq;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+        private string fn_getpath()
+        {
+            string filePath = Process.GetCurrentProcess().MainModule.FileName;
+            int index = filePath.LastIndexOf("\\");
+            if (index >= 0)
+            {
+                filePath = filePath.Substring(0, index);
+            }
+            return(filePath);
+        }
+      
+        public string getPersianNumber(string data)
+
+        {
+
+            for (int i = 48; i < 58; i++)
+            {
+                data = data.Replace(Convert.ToChar(i), Convert.ToChar(1728 + i));
+            }
+         return data;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void fn_create_report(string fname)
+        {
+            //MessageBox.Show(fname);
+            //return;
+            Word.Application word = new Word.Application();
+            word.Visible = true;
+            word.WindowState = Word.WdWindowState.wdWindowStateNormal;
+            Word.Document doc = word.Documents.Open(fn_getpath() + @"\gozaresh.docx");
+
+            string filePath = fn_getpath() + @"\"+fname+".txt";
+            
+            StreamReader reader = new StreamReader(filePath);
+            string a = "";
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (a == "")
+                {
+                    a = line;
+                }
+                else
+                {
+
+                    string vahid = getPersianNumber(line); //culture.NumberFormat.NumberToString(1944, NumberStyles.Any, culture);
+                    vahid = FN_reverser(vahid);
+                    doc.Range().Find.Execute(FindText: a, ReplaceWith: vahid, Replace: Word.WdReplace.wdReplaceAll);
+                    a = "";
+                }
+            }
+            reader.Close();
+            File.Delete(fn_getpath() + @"\" + fname + ".txt");
+
+
+            doc.SaveAs2(fn_getpath() + @"\"+fname+".docx");
+            doc.Close();
+            word.Quit();
+
+        }
+        private void fn_get_list_config()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            // دریافت لیست فایل های موجود در مسیر برنامه
+            string[] files = Directory.GetFiles(path);
+
+            // چاپ نام فایل های با پسوند txt
+            listBox1.Items.Clear();
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == ".txt")
+                {
+                    //Console.WriteLine(file);
+                    string fname = Path.GetFileNameWithoutExtension(file);
+                    if ( fname== "config")continue;
+                    Thread.Sleep(3000);
+                    fn_create_report(fname);
+                    listBox1.Items.Add(file);
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            fn_get_list_config();
+            timer1.Enabled = true;
+
+        }
+        private string FN_reverser(string str)
+        {
+            //string str = "1944/04/08/01/05/02";
+            if (str.IndexOf('/') != -1)
+            {
+                string[] parts = str.Split('/');
+                Array.Reverse(parts);
+                string reversedDate = string.Join("/", parts);
+                return (reversedDate);
+            }
+            else { 
+                return str;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+    }
+}
